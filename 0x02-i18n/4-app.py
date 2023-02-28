8240eaf2f2b1ae0a678500c2e7cfe8c8dc762722
solomonkifle <102066454+solomonkifle@users.noreply.github.com>
2023-02-28 20:35:47 +0300

    Update 4-app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
-""" 2. get local from request """
-from flask import Flask, render_template, request
+"""
+Flask app
+"""
+from flask import (
+    Flask,
+    render_template,
+    request
+)
 from flask_babel import Babel
 
 
-class Config:
-    """ Config class for app """
+class Config(object):
+    """
+    Configuration for Babel
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -13,25 +21,27 @@ class Config:
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale() -> str:
-    """ Get locale from request """
-    query_strings = request.query_string.decode('utf-8').split('&')
-    query = {k: v for k, v in [i.split('=') for i in query_strings]}
-    if 'locale' in query and query['locale'] in app.config["LANGUAGES"]:
-        return query['locale']
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+def get_locale():
+    """
+    Select and return best language match based on supported languages
+    """
+    loc = request.args.get('locale')
+    if loc in app.config['LANGUAGES']:
+        return loc
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route("/")
-def welcome() -> str:
-    """ / page """
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Handles / route
+    """
     return render_template('4-index.html')
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
